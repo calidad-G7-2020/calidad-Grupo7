9657266e83b5e03dff62528f136c416a30875ce7
Victorlopezro <victorlopz16@hotmail.com>
2020-04-20 18:44:59 +0200

    Create ExitActivitySteps.java

---
+++ app/src/androidTest/java/com/mainpakage/tetrix/test/ExitActivitySteps.java
@@ -0,0 +1,28 @@
+package com.mainpakage.tetrix.test;
+
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+//@RunWith(Cucumber.class)
+public class ExitActivitySteps {
+
+
+    @Given("^I'm in the middle of a game and I want to go out$")
+    public void im_in_the_middle_of_a_game_and_i_want_to_go_out() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^I press the exit button$")
+    public void i_press_the_exit_button() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^ I go back to the main menu$")
+    public void i_go_back_to_the_main_menu() throws Throwable {
+        throw new PendingException();
+    }
+
+}
