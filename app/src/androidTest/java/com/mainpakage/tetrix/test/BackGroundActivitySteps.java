c04a233181b7623426ba53d5696b2436ab3ebaf4
Wilhelm-mar <45694504+Wilhelm-mar@users.noreply.github.com>
2020-04-20 18:27:31 +0200

    Add BackGroundActivitySteps file

---
+++ app/src/androidTest/java/com/mainpakage/tetrix/test/BackGroundActivitySteps.java
@@ -0,0 +1,30 @@
+package com.mainpakage.tetrix.test;
+
+
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+
+
+//@RunWith(Cucumber.class)
+public class BackGroundActivitySteps {
+
+    @Given("^I'm in the menu of the game and I want to change background color$")
+    public void im_in_the_menu_of_the_game_and_i_want_to_change_background_color() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^I select the background in select bar$")
+    public void i_select_the_background_in_select_bar() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^ I change the background color$")
+    public void i_change_the_background_color() throws Throwable {
+        throw new PendingException();
+    }
+
+}
