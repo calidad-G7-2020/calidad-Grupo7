4fbff6fcf15d74e6a501a58166de1a7f291dd736
alicia <aliholi444@gmail.com>
2020-04-20 18:37:38 +0200

    Added Sound100PointsActivitySteps

---
+++ app/src/androidTest/java/com/mainpakage/tetrix/test/Sound100PointsActivitySteps.java
@@ -0,0 +1,26 @@
+package com.mainpakage.tetrix.test;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+//@RunWith(Cucumber.class)
+public class Sound100PointsActivitySteps {
+
+    @Given("^I don´t be alert each 100 points$")
+    public void i_dont_be_alert_each_100_points() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^application notifies me$")
+    public void application_notifies_me() throws Throwable {
+        throw new PendingException();
+    }
+
+    @Then("^I am motivated$")
+    public void i_am_motivated() throws Throwable {
+        throw new PendingException();
+    }
+
+}
