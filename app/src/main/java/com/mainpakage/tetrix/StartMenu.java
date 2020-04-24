b24ffb48faa676be8f3de93a6b9e9a3520f42114
Wilhelm-mar <45694504+Wilhelm-mar@users.noreply.github.com>
2020-04-24 18:34:05 +0200

    Complete background scenario with test

--- app/src/main/java/com/mainpakage/tetrix/StartMenu.java
+++ app/src/main/java/com/mainpakage/tetrix/StartMenu.java
@@ -5,13 +5,21 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StartMenu extends AppCompatActivity {
     int thm;
     int gameMode;
     boolean themeSelected;
+
+    private List<String> backGroundList;
+    private int backGroundNumber = 0;
+    private ImageView backGroundShow;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,13 @@ public class StartMenu extends AppCompatActivity {
         final Button butCl = (Button) findViewById(R.id.butClassic);
         final Button butSp = (Button) findViewById(R.id.butSpooky);
         final Button butTut = (Button) findViewById(R.id.butTutorial);
+
+        this.backGroundNumber =  getIntent().getIntExtra("paleta",0);
+
+        this.backGroundShow = findViewById(R.id.colorView);
+        this.backGroundShow.setImageResource(R.drawable.bgcl6);
+
+        this.iniciarbackGroundList();
 
         butCl.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -35,13 +50,13 @@ public class StartMenu extends AppCompatActivity {
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {    //suelto
                     Intent intent = new Intent(v.getContext(), MainActivity.class);
                     if(!themeSelected) {
-                        thm = 0;
                         themeSelected=true;
                         butCl.setBackgroundResource(R.drawable.challenge);
                         butSp.setBackgroundResource(R.drawable.train);
 
                     }else{
                         gameMode=1;
+                        thm = backGroundNumber;
                         intent.putExtra("theme", thm);
                         intent.putExtra("GameMode", gameMode);
                         startActivityForResult(intent, 0);
@@ -75,13 +90,13 @@ public class StartMenu extends AppCompatActivity {
                     Intent intent = new Intent(v.getContext(), MainActivity.class);
                     if(!themeSelected){
                         butSp.setBackgroundResource(R.drawable.choosespooky);
-                        thm = 1;
                         themeSelected=true;
                         butCl.setBackgroundResource(R.drawable.challenge);
                         butSp.setBackgroundResource(R.drawable.train);
                     }
                     else{
                         gameMode=0;
+                        thm=(backGroundNumber==0)?1:backGroundNumber;
                         intent.putExtra("theme", thm);
                         intent.putExtra("GameMode", gameMode);
                         startActivityForResult(intent, 0);
@@ -110,6 +125,57 @@ public class StartMenu extends AppCompatActivity {
                 return false;
             }
         });
+    }
+    
+    private void iniciarbackGroundList(){
+        this.backGroundList = new ArrayList<>();
+        this.backGroundList.add("0");
+        this.backGroundList.add("1");
+        this.backGroundList.add("2");
+        this.backGroundList.add("3");
+        this.backGroundList.add("4");
+
+    }
+
+    public void cambiarImagen(){
+        switch (this.backGroundNumber){
+            case 0:
+                this.backGroundShow.setImageResource(R.drawable.bgcl6);
+                break;
+            case 1:
+                this.backGroundShow.setImageResource(R.drawable.bgsp0);
+                break;
+            case 2:
+                this.backGroundShow.setImageResource(R.drawable.bgcl61);
+                break;
+            case 3:
+                this.backGroundShow.setImageResource(R.drawable.bgcl62);
+                break;
+            case 4:
+                this.backGroundShow.setImageResource(R.drawable.bgcl63);
+                break;
+            default:
+                this.backGroundShow.setImageResource(R.drawable.bgcl6);
+        }
+    }
+
+    public void previous(View view){
+        if(this.backGroundNumber>0){
+            this.backGroundNumber--;
+        }else{
+            this.backGroundNumber = this.backGroundList.size();
+        }
+
+        this.cambiarImagen();
+    }
+
+    public void next(View view){
+        if(this.backGroundNumber < backGroundList.size()){
+            this.backGroundNumber++;
+        }else{
+            this.backGroundNumber = 0;
+        }
+        this.cambiarImagen();
     }
 
     @Override
